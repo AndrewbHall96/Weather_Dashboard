94e454d895ae2f3dee2f809f8b8be83e468143bb
Andrew Hall <andrewbhall96@gmail.com>
2020-06-16 22:35:59 -0400

    updating js script. Displays 5 day forecast. Updating index.html layout

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,45 @@
 
 // Example queryURL for Giphy API
 
-  var queryURL = "api.openweathermap.org/data/2.5/forecast?q=Columbus,US-OH&appid=de4b0f913f8fa17d4d22986f83ad6fb3";
 
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(weatherResponse) {
-      console.log(weatherResponse)
+var cityName = 'Columbus';
+var stateCode = 'US-OH';
+var apiKey = 'de4b0f913f8fa17d4d22986f83ad6fb3';
+
+var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName},${stateCode}&appid=${apiKey}`;
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+
+  //naming the information coming in as "weatherResponse"
+}).then(function (weatherResponse) {
+  console.log(weatherResponse)
+
+  for (var index = 3; index < weatherResponse.list.length; index = index+8) {
+
+    var weatherInfo = weatherResponse.list[index]
+    console.log(weatherInfo)
+    console.log(weatherInfo.dt_txt)
+    // Convert the temp to fahrenheit
+    var tempF = (weatherInfo.main.temp - 273.15) * 1.80 + 32;
+    console.log(tempF)
+    console.log(weatherInfo.main.humidity)
+
+  var foreCastCard = $("<div>");
+  var foreCastH1 = $("<h1>").text(weatherInfo.dt_txt);
+  var foreCastTemp = $("<h3>").text(tempF.toFixed(2));
+  var foreCastHum = $("<h3>").text(weatherInfo.main.humidity);
+  foreCastCard.append(foreCastH1);
+  foreCastCard.append(foreCastTemp);
+  foreCastCard.append(foreCastHum);
+  $("#forecastRow").append(foreCastCard)
   
-  });
+
+
+
+  }
+  localStorage.setItem("weatherInfo", weatherResponse);
+}).then()
+
+
